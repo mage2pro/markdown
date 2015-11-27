eebb8637d566d48c6f74d8bb191dd38247580638
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-11-27 06:56:39 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -9,7 +9,7 @@ class Settings extends \Df\Core\Settings {
 	 * @used-by \Df\Core\Settings::v()
 	 * @return string
 	 */
-	protected function prefix() {return 'dfe_backend/markdown/';}
+	protected function prefix() {return 'df_backend/markdown/';}
 
 	/** @return \Dfe\Markdown\Settings */
 	public static function s() {static $r; return $r ? $r : $r = df_o(__CLASS__);}
