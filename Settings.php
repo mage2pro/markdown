9e2796fdb696da9a840b4761abeb859bdc7f3c73
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-04 11:01:21 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -2,11 +2,9 @@
 namespace Dfe\Markdown;
 /** @method static Settings s() */
 class Settings extends \Df\Core\Settings {
-	/** @return bool */
-	public function enable() {return $this->b('enable');}
-
 	/**
 	 * @override
+	 * @see \Df\Core\Settings::prefix()
 	 * @used-by \Df\Core\Settings::v()
 	 * @return string
 	 */
