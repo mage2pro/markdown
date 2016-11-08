28ae5288983310608bf4cba4d7384fd7550485f5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-08 06:51:17 +0300

    no message

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -11,6 +11,10 @@ class InstallSchema extends \Df\Framework\SetupA\Schema {
 	protected function _process() {
 		$this->createTableEav(self::TABLE_CATEGORY, 'catalog_category_entity_text');
 		$this->createTableEav(self::TABLE_PRODUCT, 'catalog_product_entity_text');
+		/**
+		 * 2016-11-04
+		 * У нас теперь также есть функция @see df_db_column_add()
+		 */
 		$this->c()->addColumn(df_table('cms_block'), self::F__MARKDOWN, 'text');
 		$this->c()->addColumn(df_table('cms_page'), self::F__MARKDOWN, 'text');
 	}
