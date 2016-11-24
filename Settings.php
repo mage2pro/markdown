a2ac2edc219f52531c3f305559c42828e3d96e4b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-24 03:46:18 +0300

    1.0.11

--- Settings.php
+++ Settings.php
@@ -1,12 +1,12 @@
 <?php
 namespace Dfe\Markdown;
 /** @method static Settings s() */
-final class Settings extends \Df\Core\Settings {
+final class Settings extends \Df\Config\Settings {
 	/**
 	 * @override
-	 * @see \Df\Core\Settings::prefix()
-	 * @used-by \Df\Core\Settings::v()
+	 * @see \Df\Config\Settings::prefix()
+	 * @used-by \Df\Config\Settings::v()
 	 * @return string
 	 */
-	protected function prefix() {return 'df_backend/markdown/';}
+	protected function prefix() {return 'df_backend/markdown';}
 }
