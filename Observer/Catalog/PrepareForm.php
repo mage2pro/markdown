3f7b4af14ed2111a1d03b9753c950964982788b2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-30 23:16:36 +0300

    no message

--- Observer/Catalog/PrepareForm.php
+++ Observer/Catalog/PrepareForm.php
@@ -2,6 +2,7 @@
 namespace Dfe\Markdown\Observer\Catalog;
 use Dfe\Markdown\CatalogAction;
 use Dfe\Markdown\CatalogAction\DbRecord;
+use Dfe\Markdown\FormElement as FE;
 use Magento\Framework\Event\Observer as O;
 use Magento\Framework\Event\ObserverInterface;
 /**
@@ -9,11 +10,12 @@ use Magento\Framework\Event\ObserverInterface;
  * Обработка события «adminhtml_catalog_category_edit_prepare_form».
  * Цель обработки события — подстановка Markdown вместо HTML в поля редакторов
  * на административной странице товарного раздела.
- * https://github.com/magento/magento2/blob/c743dec47b2e5de036eb5638fec44a54bfb31040/app/code/Magento/Catalog/Block/Adminhtml/Category/Tab/Attributes.php#L112
-		$form->addValues($this->getCategory()->getData());
-		$this->_eventManager->dispatch(
-			'adminhtml_catalog_category_edit_prepare_form', ['form' => $form]
-		);
+ * https://github.com/magento/magento2/blob/c743dec47b2e5de036eb5638fec44a54bfb31040/app/code/Magento/Catalog
+/Block/Adminhtml/Category/Tab/Attributes.php#L112
+ *		$form->addValues($this->getCategory()->getData());
+ *		$this->_eventManager->dispatch(
+ *			'adminhtml_catalog_category_edit_prepare_form', ['form' => $form]
+ *		);
  */
 /**
  * 2015-11-03
@@ -45,8 +47,8 @@ final class PrepareForm implements ObserverInterface {
 			/** @var \Magento\Framework\Data\Form\Element\Fieldset $fieldset */
 			$fieldset = $form->getElements()[0];
 			foreach ($fieldset->getElements() as $element) {
-				/** @var \Magento\Framework\Data\Form\Element\AbstractElement|\Dfe\Markdown\FormElement $element */
-				if ($element instanceof \Dfe\Markdown\FormElement) {
+				/** @var \Magento\Framework\Data\Form\Element\AbstractElement|FE $element */
+				if ($element instanceof FE) {
 					/**
 					 * 2015-11-03
 					 * Здесь надо вызывать именно $element['name'], а не $element->getName(),
