7c71b529d0980c61ff390570f3147b14718cf547
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-27 17:26:41 +0600

    1.1.2: https://github.com/mage2pro/markdown/issues/3

--- Modifier.php
+++ Modifier.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dfe\Markdown;
+use Magento\Catalog\Model\Product as P;
 use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
 /**
  * 2016-02-25
@@ -14,23 +15,29 @@ class Modifier extends AbstractModifier {
 	 * @param array(string => mixed) $data
 	 * @return array(string => mixed) $data
 	 */
-	function modifyData(array $data) {return
+	function modifyData(array $data) {
 		/**
 		 * 2018-09-27
-		 * Magento 2.3 calls the `modifyData` method on the backend product list,
+		 * 1) Magento 2.3 calls the `modifyData` method on the backend product list,
 		 * and it leads to the error «The product wasn't registered»:
 		 * https://github.com/mage2pro/markdown/issues/2
-		 * @see \Magento\Catalog\Model\Locator\RegistryLocator::getProduct
+		 * @see \Magento\Catalog\Model\Locator\RegistryLocator::getProduct()
+		 * 2) `df_action_is('catalog_product_index')` does not help here,
+		 * because the `modifyData` method is called via AJAX too, and the action name is `mui_index_render`.
+		 * "Magento 2.3: «Something went wrong with processing current custom view
+		 * and filters have been reset to its original state. Please edit filters then click apply.":
+		 * https://github.com/mage2pro/markdown/issues/3
 		 */
-		!Settings::s()->enable() || df_action_is('catalog_product_index') ? $data : array_replace_recursive($data, [
+		$p = df_product_current(); /** @var P|null $p */
+		return !$p || !Settings::s()->enable() ? $data : array_replace_recursive($data, [
 			df_catalog_locator()->getProduct()->getId() => [
 				self::DATA_SOURCE_DEFAULT => df_clean([
 					'description' => DbRecord::load('description')
 					,'short_description' => DbRecord::load('short_description')
 				])
 			]
-		])
-	;}
+		]);
+	}
 
 	/**
 	 * 2016-02-25
