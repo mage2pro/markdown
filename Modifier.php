c656c689963b8f8c31b5e5b6e35835c930ecfc84
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-15 15:06:26 +0600

    1.1.7

--- Modifier.php
+++ Modifier.php
@@ -1,6 +1,5 @@
 <?php
 namespace Dfe\Markdown;
-use Magento\Catalog\Model\Product as P;
 use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
 /**
  * 2016-02-25
@@ -12,10 +11,10 @@ class Modifier extends AbstractModifier {
 	 * @override
 	 * @see \Magento\Ui\DataProvider\Modifier\ModifierInterface::modifyData()
 	 * https://github.com/magento/magento2/blob/e0ed4bad/app/code/Magento/Ui/DataProvider/Modifier/ModifierInterface.php#L13-L17
-	 * @param array(string => mixed) $data
-	 * @return array(string => mixed) $data
+	 * @param array(string => mixed) $v
+	 * @return array(string => mixed)
 	 */
-	function modifyData(array $data) {
+	function modifyData(array $v) {
 		/**
 		 * 2018-09-27
 		 * 1) Magento 2.3 calls the `modifyData` method on the backend product list,
@@ -28,13 +27,15 @@ class Modifier extends AbstractModifier {
 		 * and filters have been reset to its original state. Please edit filters then click apply.":
 		 * https://github.com/mage2pro/markdown/issues/3
 		 */
-		$p = df_product_current(); /** @var P|null $p */
-		return !$p || !Settings::s()->enable() ? $data : array_replace_recursive($data, [$p->getId() => [
-			self::DATA_SOURCE_DEFAULT => df_clean([
-				'description' => DbRecord::load('description')
-				,'short_description' => DbRecord::load('short_description')
-			])
-		]]);
+		;
+		return !($id = df_product_current_id() /** @var int|null $id */) || !Settings::s()->enable()
+			? $v : array_replace_recursive($v, [$id => [
+				self::DATA_SOURCE_DEFAULT => df_clean([
+					'description' => DbRecord::load('description')
+					, 'short_description' => DbRecord::load('short_description')
+				])
+			]])
+		;
 	}
 
 	/**
@@ -42,10 +43,10 @@ class Modifier extends AbstractModifier {
 	 * @override
 	 * @see \Magento\Ui\DataProvider\Modifier\ModifierInterface::modifyMeta()
 	 * https://github.com/magento/magento2/blob/e0ed4bad/app/code/Magento/Ui/DataProvider/Modifier/ModifierInterface.php#L19-L23
-	 * @param array(string => mixed) $meta
-	 * @return array(string => mixed) $meta
+	 * @param array(string => mixed) $v
+	 * @return array(string => mixed)
 	 */
-	function modifyMeta(array $meta) {return $meta;}
+	function modifyMeta(array $v) {return $v;}
 }
 
 
