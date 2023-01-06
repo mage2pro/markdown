dc17c3983de8d3965a36af710b362bfb3a19f2d9
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-06 18:43:59 +0000

    https://github.com/mage2pro/core/issues/168

--- Observer/Cms/Predispatch.php
+++ Observer/Cms/Predispatch.php
@@ -24,7 +24,7 @@ final class Predispatch implements ObserverInterface {
 	 * @see ObserverInterface::execute()
 	 * @used-by \Magento\Framework\Event\Invoker\InvokerDefault::_callObserverMethod()
 	 */
-	function execute(O $o) {
+	function execute(O $o):void {
 		if (\Dfe\Markdown\Settings::s()->enable()) {
 			/** @var \Magento\Framework\App\RequestInterface|\Magento\Framework\App\Request\Http $request */
 			$request = $o['request'];
